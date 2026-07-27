c4cfe2e17709bf5f4c9463e16aef75d8438e84a0
Riley Georgen <rgeorgen10@gmail.com>
2026-07-26 20:12:54 -0600

    Add car-counter.py

---
+++ car-counter.py
@@ -0,0 +1,8 @@
+main()  # Call the Main Function
+
+class Car: # Car class: stores the car number and the number of votes
+
+def main() {   # The Main Function: Where the program is mainly operated
+    
+    exit(0)
+}
